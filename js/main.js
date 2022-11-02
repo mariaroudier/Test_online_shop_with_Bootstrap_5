bca3de84d9986a903c4261bfa8ecbf08c158d907
mariaroudier <volokh.maria@gmail.com>
2022-11-02 17:05:44 +0100

    added a function of deleting the items

--- js/main.js
+++ js/main.js
@@ -1,12 +1,18 @@
 //const bootstrap = require("bootstrap")
 //const { default: Swal } = require("sweetalert2")
 
+// const { default: Swal } = require("sweetalert2")
+
 if(!localStorage.getItem('goods')) {
       localStorage.setItem('goods', JSON.stringify([]))
 }
 let myModal = new bootstrap.Modal(document.getElementById('exampleModal'), {
       keyboard: false
 })
+let options = {
+      valueNames: ['name','price']
+}
+let userList;
 
 document.querySelector('button.add_new').addEventListener('click', function (e) {
       let name = document.getElementById('good_name').value
@@ -48,7 +54,7 @@ function update_goods() {
                         <td class="price">${goods[i][2]}</td>
                         <td>${goods[i][3]}</td>
                         <td><button class="good_delete btn-danger" data-delete="${goods[i][0]}">&#10006;</button></td>
-                        <td><button class="good_delete btn-danger" data-goods="${goods[i][0]}">&#10149;</button></td>
+                        <td><button class="good_delete btn-primary" data-goods="${goods[i][0]}">&#10149;</button></td>
                   </tr>
 
                   `)
@@ -71,9 +77,45 @@ function update_goods() {
 
                   }
             }
-            // userlist = new List('goods', options);
+            userList = new List('goods', options);
+      } else {
+            table1.hidden = true
+            table2.hidden = true
       }
+      document.querySelector('.price_result').innerHTML = result_price + '$'
 
 }
 
 update_goods()
+
+document.querySelector('.list').addEventListener('click', function(e){
+      if(!e.target.dataset.delete) {
+            return
+      }
+      Swal.fire({
+            title: 'Attention!',
+            text: 'Are you sure you want to delete this item?',
+            icon: 'warning',
+            showCancelButton:true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes',
+            cancelButtonText: 'Cancel',
+      }).then((result) => {
+            if(result.isConfirmed) {
+                  let goods = JSON.parse(localStorage.getItem('goods'))
+                  for(let i=0; i<goods.length; i++) {
+                        if(goods[i][0] == e.target.dataset.delete) {
+                              goods.splice(i, 1)
+                              localStorage.setItem('goods', JSON.stringify(goods))
+                              update_goods()
+                        }
+                  }
+                  Swal.fire(
+                        "Deleted",
+                        "The selected product has been successfully deleted",
+                        "Success"
+                  )
+            }
+      })
+})
