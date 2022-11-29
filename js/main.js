d9e97301b381e6adb62cccae59712c2919ef4d13
mariaroudier <volokh.maria@gmail.com>
2022-11-29 17:24:13 +0100

    finished sorting and discount

--- js/main.js
+++ js/main.js
@@ -2,6 +2,40 @@
 //const { default: Swal } = require("sweetalert2")
 
 // const { default: Swal } = require("sweetalert2")
+table1.onclick = function(e) {
+      if(e.target.tagName != 'TH') return
+      let th = e.target
+      sortTable(th.cellIndex, th.dataset.type, 'table1')
+}
+table2.onclick = function(e) {
+      if(e.target.tagName != 'TH') return
+      let th = e.target
+      sortTable(th.cellIndex, th.dataset.type, 'table2')
+}
+function sortTable(colNum, type, id) {
+      let elem = document.getElementById(id)
+      let tbody = elem.querySelector('tbody')
+      let rowsArray = Array.from(tbody.rows)
+      let compare
+      switch (type) {
+            case 'number':
+                  compare = function(rowA,rowB) {
+                        return rowA.cells[colNum].innerHTML - rowB.cells[colNum].innerHTML
+                  }
+                  break;
+      
+            case 'string':
+                  compare = function(rowA,rowB) {
+                        return rowA.cells[colNum].innerHTML > rowB.cells[colNum].innerHTML ? 1 : -1
+                  }
+                  break
+
+      }
+      rowsArray.sort(compare)
+      tbody.append(...rowsArray)
+
+}
+
 
 if(!localStorage.getItem('goods')) {
       localStorage.setItem('goods', JSON.stringify([]))
@@ -23,7 +57,7 @@ document.querySelector('button.add_new').addEventListener('click', function (e) 
             document.getElementById('good_price').value = ''
             document.getElementById('good_count').value = '1'
             let goods = JSON.parse(localStorage.getItem('goods'))
-            goods.push(['good_' +goods.length, name, price, count, 0, 0, 0])
+            goods.push(['good_' + (goods.length+1), name, price, count, 0, 0, 0])
             localStorage.setItem('goods', JSON.stringify(goods))
             update_goods()
             myModal.hide()
@@ -47,18 +81,20 @@ function update_goods() {
             table2.hidden = false
             for(let i = 0; i < goods.length;i++) {
                   tbody.insertAdjacentHTML('beforeend',
+                  // [i][3] is quantity
                   `
                   <tr class = "align-middle">
                         <td>${i+1}</td>
                         <td class="name">${goods[i][1]}</td>
                         <td class="price">${goods[i][2]}</td>
-                        <td>${goods[i][3]}</td>
+                        <td>${goods[i][3]}</td> 
                         <td><button class="good_delete btn-danger" data-delete="${goods[i][0]}">&#10006;</button></td>
                         <td><button class="good_delete btn-primary" data-goods="${goods[i][0]}">&#10149;</button></td>
                   </tr>
 
                   `)
-                  if(goods[i][4] > 0) {
+                  if(goods[i][4] > 0) { // quantity of items in the cart
+                       //  goods[i][6]  - total sum
                         goods[i][6] = goods[i][4] * goods[i][2] - goods[i][4] * goods[i][2] * goods[i][5] * 0.01
                         result_price += goods [i][6]
                         document.querySelector('.cart').insertAdjacentHTML('beforeend', 
@@ -67,14 +103,15 @@ function update_goods() {
                               <td>${i+1}</td>
                               <td class="price_name">${goods[i][1]}</td>
                               <td class="price_one">${goods[i][2]}</td>
-                              <td class="price_count>${goods[i][4]}</td>
-                              <td class="price_discount"><input data-goodid="${goods[i][0]}" type="text" value = "${goods[i][5]}" min="0"  max="100"></td>
+                              <td class="price_count">${goods[i][4]}</td>
+                              <td class="price_discount"><input data-goodid="${goods[i][0]}" max="100" min="0" type=text value=${goods[i][5]}></td>
                               <td>${goods[i][6]}</td>
                               <td><button class="good_delete btn-danger" data-delete="${goods[i][0]}">&#10006;</button></td>
                               
+                              
+                              
                         </tr>
                         `)
-
                   }
             }
             userList = new List('goods', options);
@@ -118,4 +155,55 @@ document.querySelector('.list').addEventListener('click', function(e){
                   )
             }
       })
+})
+
+document.querySelector('.list').addEventListener('click', function(e){
+      if(!e.target.dataset.goods) {
+            return
+      }
+      let goods = JSON.parse(localStorage.getItem('goods'))
+      for(let i=0; i<goods.length; i++) {
+            if(goods[i][3]>0 && goods[i][0] == e.target.dataset.goods) {
+                  goods[i].splice(3,1, goods[i][3]-1)
+                  goods[i].splice(4,1, goods[i][4]+1)
+                  localStorage.setItem('goods', JSON.stringify(goods))
+                  update_goods()
+            }
+      }
+
+})
+
+document.querySelector('.cart').addEventListener('click', function(e){
+      if(!e.target.dataset.delete) {
+            return
+      }
+      let goods = JSON.parse(localStorage.getItem('goods'))
+      for(let i=0; i<goods.length; i++) {
+            if(goods[i][4]>0 && goods[i][0] == e.target.dataset.delete) {
+                  goods[i].splice(3,1, goods[i][3]+1)
+                  goods[i].splice(4,1, goods[i][4]-1)
+                  localStorage.setItem('goods', JSON.stringify(goods))
+                  update_goods()
+            }
+      }
+
+})
+document.querySelector('.cart').addEventListener('input', function(e){
+      if(!e.target.dataset.goodid) {
+            console.log(false)
+
+      }
+      let goods = JSON.parse(localStorage.getItem('goods'))
+      console.log(goods.length)
+      for(let i=0; i<goods.length; i++) {
+            if(goods[i][0] == e.target.dataset.goodid) {
+                  goods[i][5] = e.target.value
+                  goods[i][6] = goods[i][4] * goods[i][2] - goods[i][4] * goods[i][2] * goods[i][5] * 0.01
+                  localStorage.setItem('goods', JSON.stringify(goods))
+                  update_goods()
+                  let input = document.querySelector(`[data-goodid="${goods[i][0]}"]`)
+                  input.focus()
+                  input.selectionStart = input.value.length
+            }
+      }
 })
